2566b5bc9aa7773ed3d4fa640b7fd211fdd31a14
VoxeveR <piotrzietek01@gmail.com>
2025-03-08 22:48:11 +0100

    update: K2025-11 add new questions

--- app/2025/containers/Questions/data/index.ts
+++ app/2025/containers/Questions/data/index.ts
@@ -2,51 +2,56 @@ export const questionsData = [
   {
     id: '2025-question-1',
     title: 'Kiedy, co i gdzie to się odbywa?',
-    text: 'Hackathon oraz inne atrakcje odbędą się 31 maja o godzinie 11:00 w Klubie Kwadrat w Krakowie. Zachęcamy do śledzenia naszych mediów spółecznościowych, w razie wystąpienia jakichś zmian.',
+    text: 'Hackathon oraz inne atrakcje odbędą się 31 maja o godzinie 10:30 w Klubie Kwadrat w Krakowie. Zachęcamy do śledzenia naszych mediów społecznościowych, w razie wystąpienia zmian.',
   },
   {
     id: '2025-question-2',
     title: 'Czy na wydarzenie obowiązują zapisy? Kto może się zapisać?',
-    text: 'Zapisy są na Hackathon, do których link jest dostępny powyżej. Mogą obowiązywać zapisy na niektóre konferencje - szczegóły również pojawią się na stronie WWW i stronie wydarzenia.',
+    text: 'Zapisy na Hackathon są obowiązkowe, link do formularza dostępny jest na stronie.',
   },
   {
     id: '2025-question-3',
+    title: 'Jaki będzie temat hackathonu?',
+    text: 'Temat zostanie ujawniony podczas wydarzenia. 😉 ',
+  },
+  {
+    id: '2025-question-4',
     title: 'Czy mogę się zapisać samemu, czy muszę mieć drużynę?',
     text: 'Na Hackathon możesz zapisać się zarówno indywidualnie, jak i drużynowo. Jeśli zdecydujesz się na zapis solo, poinformujemy Cię o sposobie przydzielania do drużyn.',
   },
   { // ZMIEN
-    id: '2025-question-4',
-    title: 'Czy udział w wydarzeniu jest bezpłatny?',
-    text: 'Tak, udział w Hackathonie jak i we wszystkich konferencjach i atrakcjach jest bezpłatny. Warunkiem uczestnictwa może być konieczność wcześniejszej rejestracji.',
-  },
-  {
     id: '2025-question-5',
-    title: 'Czy turniej jest dla mnie?',
-    text: 'Jeśli masz choćby podstawową wiedzę w zakresie programowania lub posiadasz inne umiejętności, które mogą przydać się do stworzenia ciekawej aplikacji, to turniej jest właśnie dla Ciebie! Nie bój się spróbować – udział jest bezpłatny, a możesz zdobyć fantastyczne nagrody.',
+    title: 'Czy udział w wydarzeniu jest bezpłatny?',
+    text: 'Tak, udział w Hackathonie jest bezpłatny. Warunkiem uczestnictwa jest wcześniejsza rejestracja.',
   },
   {
-    id: "2025-question-6",
-    title: 'Co muszę mieć ze sobą?',
-    text: 'Laptop i zasilacz, ewentualnie powerbank (w razie awarii) to absolutna konieczność. Przydatne może być też założenie sobie konta na platformie GitHub. Przygotuj się na 24 godzinny maraton programowania, dlatego warto też zadbać o ewentualny wypoczynek (zalecane wzięcie koców, poduszek lub śpiworów). Na wydarzeniu będzie sala, na której będzie można odpocząć i uciąć drzemkę. Chyba nie musimy wspominać o dużej porcji motywacji i pomysłów na projekt?',
+    id: '2025-question-6',
+    title: 'Czy turniej jest dla mnie?',
+    text: 'Jeśli masz choćby podstawową wiedzę w zakresie programowania lub posiadasz inne umiejętności, które mogą przydać się do stworzenia ciekawej aplikacji, to turniej jest właśnie dla Ciebie! Nie bój się spróbować – udział jest bezpłatny, a możesz zdobyć fantastyczne nagrody i świetnie się bawić.',
   },
   {
     id: "2025-question-7",
-    title: 'A co z jedzeniem?',
-    text: 'Dla uczestników turnieju jest zapewnione jedzenie: pizza, jeden ciepły posiłek, napoje i energetyki. Dodatkowo na miejscu będzie możliwość kupienia sobie dodatkowych przekąsek.',
+    title: 'Co muszę mieć ze sobą?',
+    text: 'Laptop, zasilacz i powerbank (w razie awarii) to absolutna konieczność. Przydatne może być też założenie sobie konta na platformie GitHub. Przygotuj się na 24-godzinny maraton programowania, dlatego warto też zadbać o ewentualny wypoczynek (zalecane wzięcie koców, poduszek lub śpiworów). Chyba nie musimy wspominać o dużej porcji motywacji i pomysłów na projekt?',
   },
   {
     id: "2025-question-8",
-    title: 'Kto może wziąć udział w wydarzeniu?',
-    text: 'Uczestnicy Hackathonu muszą być osobami pełnoletnimi posiadającymi ważny status studenta lub legitymacje szkolną. Natomiast na same wydarzenie zapraszamy wszystkich zainteresowanych, niezależnie od wieku.',
+    title: 'A co z jedzeniem?',
+    text: 'Dla uczestników turnieju jest zapewnione jedzenie: pizza, jeden ciepły posiłek, napoje (w tym pobudzające 😉). Dodatkowo na miejscu będzie możliwość kupienia sobie dodatkowych przekąsek.',
   },
   {
     id: "2025-question-9",
-    title: 'Ile osób może mieć drużyna?',
+    title: 'Kto może wziąć udział w wydarzeniu?',
+    text: 'Uczestnicy Hackathonu muszą być osobami pełnoletnimi posiadającymi ważny status studenta lub legitymacje szkolną.',
+  },
+  {
+    id: "2025-question-10",
+    title: 'Ile osób może liczyć jedna drużyna?',
     text: 'Drużyny mogą liczyć od 2 do 4 osób.',
   },
   {
-    id: "2025-question-10", //SPRAWDZ MAIL
+    id: "2025-question-11",
     title: 'Mam inne pytanie',
-    text: 'Napisz do nas w wiadomości email: uczestnicy.kosciuszkon@gmail.com lub na instagramie. Ktoś z naszego teamu z chęcia rozwieje wszelkie wątpliwości.',
+    text: 'Napisz do nas w wiadomości email: kosciuszkon@samorzad.pk.edu.pl lub na instagramie. Ktoś z naszego teamu z chęcia rozwieje wszelkie wątpliwości.',
   },
 ];
