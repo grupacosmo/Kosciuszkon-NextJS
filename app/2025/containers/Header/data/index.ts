d4d2148c206e65eb248771faf56bea72432e822d
VoxeveR <piotrzietek01@gmail.com>
2025-03-20 00:53:22 +0100

    feat: K2025-27 change about/enrolment placement

--- app/2025/containers/Header/data/index.ts
+++ app/2025/containers/Header/data/index.ts
@@ -1,6 +1,6 @@
 export const localLinks = [
-  { id: '1', label: 'Zapisy', href: '#zapisy' },
-  { id: '2', label: 'O wydarzeniu', href: '#o wydarzeniu' },
+  { id: '1', label: 'O wydarzeniu', href: '#o wydarzeniu' },
+  { id: '2', label: 'Zapisy', href: '#zapisy' },
   //{ id: '3', label: 'Tematy turnieju', href: '#tematy' },
   { id: '4', label: 'Harmonogram', href: '#harmonogram' },
   { id: '5', label: 'FAQ', href: '#faq' },
