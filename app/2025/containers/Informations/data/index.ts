bbe8b594dce30341fff8ad909e46bf535cceefec
VoxeveR <piotrzietek01@gmail.com>
2025-03-25 08:03:05 +0100

    feat: K2025-43 individual slider image styling

--- app/2025/containers/Informations/data/index.ts
+++ app/2025/containers/Informations/data/index.ts
@@ -1,9 +1,12 @@
+import styles from './SliderImageStyles.module.scss';
+
 export const sliderContent = [
   {
     id: '2025-slide-1',
     image: {
       src: '/p2025/bcg/kwadrat.webp',
       alt: 'Budynek w którym mieści się studio kwadrat.',
+      imageClass: styles['slideImage1']
     },
     content: {
       title: 'Czym jest Kościuszkon?',
@@ -18,6 +21,7 @@ export const sliderContent = [
     image: {
       src: '/p2025/bcg/wydarzenie_live.webp',
       alt: 'Kamerzysta nagrywający wywiad.',
+      imageClass: styles['slideImage2']
     },
     content: {
       title: 'Hackathon',
@@ -33,6 +37,7 @@ export const sliderContent = [
     image: {
       src: '/p2025/bcg/kwadrat_napis.webp',
       alt: 'Napis "Kwadrat" na drzwiach przed wejściem do klubu.',
+      imageClass: styles['slideImage3']
     },
     content: {
       title: 'Przebieg wydarzenia',
@@ -45,6 +50,7 @@ export const sliderContent = [
     image: {
       src: '/p2025/bcg/fotoramka_gigachad_aura.webp',
       alt: 'Zdjęcie z fotoramki, osoby wskazującej palcem w stronę fotografa.',
+      imageClass: styles['slideImage4']
     },
     content: {
       title: 'Atrakcje',
@@ -61,13 +67,14 @@ export const sliderContent = [
     image: {
       src: '/p2025/bcg/prezentacja.webp',
       alt: 'Kadr z targów pracy.',
+      imageClass: styles['slideImage5']
     },
     content: {
       title: 'Sesja mentoringowa',
       text: `Podczas wydarzenia czeka na Was wyjątkowa okazja do rozmów z doświadczonymi specjalistami. 
       Sesja mentoringowa będzie nie tylko szansą na networking, ale także cennym wsparciem 
       w dopracowaniu Waszego projektu podczas hackathonu. 
-      To idealny moment, aby uzyskać wskazówki, rozwiązać problemy i wzmocnić swoją koncepcję przed prezentacją!`,
+      To idealny moment, aby uzyskać wskazówki, rozwiązać problemy i wzmocnić swoją koncepcję przed prezentacją!`
     },
   },
 ];
--- app/utils/Slider/SliderItem.tsx
+++ app/utils/Slider/SliderItem.tsx
@@ -1,57 +1,66 @@
 import { type ReactElement } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import styles from './SliderItem.module.scss';
+import clsx from 'clsx';
 
 const MOTION_CONFIG = {
-  initial: { opacity: 0 },
-  animate: { opacity: 1 },
-  transition: { duration: 0.4, ease: 'easeOut' },
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    transition: { duration: 0.4, ease: 'easeOut' },
 };
 
 type SlideImage = {
-  src: string;
-  alt: string;
+    src: string;
+    alt: string;
+    imageClass?: string; // New property for custom image-specific CSS class
 };
 
 export type SlideData = {
-  id: string;
-  image?: SlideImage;
-  content: any;
+    id: string;
+    image?: SlideImage;
+    content: any;
 };
 
 type SingleSlideProps = {
-  slide: SlideData;
-  render: (content: any) => ReactElement | null;
-  isActive: boolean;
+    slide: SlideData;
+    render: (content: any) => ReactElement | null;
+    isActive: boolean;
 };
 
 export function SingleSlide({
-  slide: { image, content },
-  render,
-  isActive,
-}: SingleSlideProps): ReactElement | null {
-  if (!isActive) {
-    return null;
-  }
+                                slide: { image, content },
+                                render,
+                                isActive,
+                            }: SingleSlideProps): ReactElement | null {
+    if (!isActive) {
+        return null;
+    }
 
-  return (
-    <>
-      {isActive && (
-        <AnimatePresence>
-          <motion.article {...MOTION_CONFIG} className={styles.slide}>
-            {image && (
-              <picture className={styles.slidePicture}>
-                <img
-                  className={styles.slideImg}
-                  src={image.src}
-                  alt={image.alt}
-                />
-              </picture>
+    return (
+        <>
+            {isActive && (
+                <AnimatePresence>
+                    <motion.article {...MOTION_CONFIG} className={styles.slide}>
+                        {image && (
+                            <picture
+                                className={clsx(
+                                    styles.slidePicture,
+                                )}
+                            >
+                                <img
+                                    className={clsx(
+                                        styles.slideImg,
+                                        image.imageClass // Add custom image-specific class
+                                    )}
+                                    src={image.src}
+                                    alt={image.alt}
+                                />
+                            </picture>
+                        )}
+                        {render(content)}
+                    </motion.article>
+                </AnimatePresence>
             )}
-            {render(content)}
-          </motion.article>
-        </AnimatePresence>
-      )}
-    </>
-  );
+        </>
+    );
 }
